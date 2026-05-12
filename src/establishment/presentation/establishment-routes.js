7e74ae0e05e29f5f23c87f389037824d337b50d2
DhilsenDevs <160343685+Dhilsen18@users.noreply.github.com>
2026-05-12 03:18:45 -0500

    feat: add detailed views for establishment, team, operator, and device

--- src/establishment/presentation/establishment-routes.js
+++ src/establishment/presentation/establishment-routes.js
@@ -3,6 +3,10 @@ const establishmentForm = () => import('./views/establishment-form.vue');
 const mapOfEstablishments = () => import('./views/map-of-establishments.vue');
 const viewEstablishments = () => import('./views/view-establishments.vue');
 const viewOperators = () => import('./views/view-operators.vue');
+const viewEstablishmentDetail = () => import('./views/view-establishment-detail.vue');
+const viewEstablishmentTeam = () => import('./views/view-establishment-team.vue');
+const viewEstablishmentOperatorDetail = () => import('./views/view-establishment-operator-detail.vue');
+const viewEstablishmentDeviceDetail = () => import('./views/view-establishment-device-detail.vue');
 
 const establishmentRoutes = [
     {
@@ -10,6 +14,30 @@ const establishmentRoutes = [
         name: 'establishment-new',
         component: establishmentForm,
         meta: { title: 'New Establishment' }
+    },
+    {
+        path: 'establishments/:establishmentId/operators/:operatorId',
+        name: 'establishment-operator-detail',
+        component: viewEstablishmentOperatorDetail,
+        meta: { title: 'Operator detail' }
+    },
+    {
+        path: 'establishments/:establishmentId/devices/:deviceId',
+        name: 'establishment-device-detail',
+        component: viewEstablishmentDeviceDetail,
+        meta: { title: 'Device detail' }
+    },
+    {
+        path: 'establishments/:establishmentId/team',
+        name: 'establishment-team',
+        component: viewEstablishmentTeam,
+        meta: { title: 'Establishment team' }
+    },
+    {
+        path: 'establishments/:establishmentId',
+        name: 'establishment-detail',
+        component: viewEstablishmentDetail,
+        meta: { title: 'Establishment detail' }
     },
     {
         path: 'establishments',
